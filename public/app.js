fa3dce5803af9a33c2215e6889471359d883f162
Rodrigo <rodmentou@gmail.com>
2015-12-10 23:33:08 -0300

    Automation and design
    - User don't need to enter first sunday nor month number, just the month
    and the year.
    - Some CSS changes on the sheet table so it won't show double borders.

--- public/app.js
+++ public/app.js
@@ -7,69 +7,12 @@ app.controller('mainController', function ($scope) {
 
   $scope.users = [
 
-  {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
-    {name: 'Rodrigo Nascimento de Souza', dpt: 'IT', role: 'Intern'},
-  {name: 'Bill Gates', dpt: 'IT', role: 'Philanthropist'},
-  {name: 'Calvin Harris', dpt: 'MUSIC', role: 'Employee'},
-  {name: 'Ness M.', dpt: 'LAKE', role: 'Monster'},
-  {name: 'Tony Stark', dpt: 'IT', role: 'Tony Stark'},
-  {name: 'Plankton', dpt: 'LAKE', role: 'Employee'},
-  {name: 'David Guetta', dpt: 'MUSIC', rol: 'Employee'},
+  {nome: 'Rodrigo Nascimento de Souza', departamento: 'GETIC', cargo: 'Handsome intern'},
+  {nome: 'Fulano da Silva Sauro', departamento: 'DINO', cargo: 'Derrubador de árvores'},
+  {nome: 'Kyocera Mita', departamento: 'GETIC', cargo: 'Impressora barulhenta'},
+  {nome: 'Baby', departamento: 'DINO', cargo: 'Não é a mamãe'},
+  {nome: 'Bill Gates', departamento: 'GETIC', cargo: 'Filântropo'}
+
 
 
 
@@ -95,7 +38,8 @@ app.controller('mainController', function ($scope) {
 
 
 
-
+  $scope.months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+                'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
   $scope.setHoliday = function(holiday) {
     if ($scope.days) {
@@ -106,10 +50,21 @@ app.controller('mainController', function ($scope) {
   };
 
   $scope.setCalendar = function(calendar) {
-    var months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-                  'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+
+    var monthNumber = $scope.months.indexOf(calendar.monthName);
+    console.log(calendar.monthName);
+    console.log(monthNumber);
+
+
+
+    $scope.month = $scope.months[monthNumber];
     $scope.year = calendar.year;
-    $scope.month = months[calendar.monthNumber-1];
+    var date = new Date($scope.year, monthNumber, 1, 0, 0, 0, 0);
+    calendar.firstSunday = +firstSunday($scope.year, monthNumber);
+    calendar.numDays = +getNumDays($scope.year, monthNumber);
+    console.log(calendar.firstSunday);
+
     $scope.days = [];
     $scope.calendarCreated = true;
     var valid, message;
@@ -119,14 +74,41 @@ app.controller('mainController', function ($scope) {
 
     for (var i = calendar.firstSunday-1; i < calendar.numDays; i+=7 ) {
       $scope.days[i].valid = false;
-      $scope.days[i].message = 'Domingo';
+      $scope.days[i].message = 'Sunday';
       if (i > 1) {
-        $scope.days[i-1].message = 'Sábado';
+        $scope.days[i-1].message = 'Saturday';
       }
     };
 
 
   };
+
+
+  var firstSunday = function (year, month){
+    var d = new Date(year, month, 1, 0, 0, 0, 0);
+    var day = 0;
+    if (d.getDay() == 0) {
+       day = 1;
+       d = d.setDate(day);
+       d = new Date(d);
+    }
+    else if (d.getDay() != 1) {
+       day = 8-(d.getDay());
+       d = d.setDate(day);
+       d = new Date(d);
+     }
+     var a = d.getDate();
+     var t = d.getDay();
+     var r = a - t;
+     return r;
+   };
+
+   var getNumDays = function (year, month) {
+     var monthStart = new Date(year, month, 1);
+     var monthEnd = new Date(year, month + 1, 1);
+     var monthLength = (monthEnd - monthStart) / (1000 * 60 * 60 * 24);
+     return monthLength;
+   };
 
 
 });
